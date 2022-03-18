7936fc9d19e2606b6587c91113ac32b072556c48
Lukas Kaul <lukas.kaul@kit.edu>
2022-03-18 06:24:54 +0100

    Add error detection to odrive setup script.

--- tests/motor_test/readme.md
+++ tests/motor_test/readme.md
@@ -1,9 +1,10 @@
 Make sure the wheels are free to turn. 
 Load this sketch on the Arduino, power up the robot, connect the Arduino via USB and open the Arduino serial terminal. 
-Enter the letter h and press ENTER A help text should appear, telling you which commands are available. 
+Enter the letter h and press ENTER. A help text should appear, telling you which commands are available. 
 
-Type ```c 0 1``` and hit ENTER to command 1.0A of current to motor 0. 
-Type ```s``` and hit ENTER to stop the motor. 
+* Type ```e``` and hit ENTER to enable the motors.
+* Type ```c 0 1``` and hit ENTER to command 1.0A of current to motor 0. 
+* Type ```s``` and hit ENTER to stop the motor. 
 Do the same for motor 1. 
 You can try higher currents, too. 
 Use the ```b``` command to command both motors at the same time.
--- tools/odrive_setup.py
+++ tools/odrive_setup.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
-# Based largely on https://docs.odriverobotics.com/hoverboard.html.
-# Developed and tested with ODrive Python 0.5.4.
+# Based largely on https://docs.odriverobotics.com/v/latest/hoverboard.html
+# Developed and tested with ODrive Python 0.5.4. and ODrive firmware version 0.5.4
 
-import odrive
 import argparse
+import odrive
+import sys
 import time
 
-def configure_motor(odrv, ax_num):
-    axes = [odrv.axis0, odrv.axis1]
-    ax = axes[ax_num]
+def configure_motor(ax):
     ax.motor.config.pole_pairs = 15
     ax.motor.config.resistance_calib_max_voltage = 6
     ax.motor.config.requested_current_range = 25
@@ -17,17 +16,13 @@ def configure_motor(odrv, ax_num):
     ax.motor.config.torque_constant = 1
     ax.motor.config.current_lim = 15
     
-def configure_encoder(odrv, ax_num):
-    axes = [odrv.axis0, odrv.axis1]
-    ax = axes[ax_num]
+def configure_encoder(ax):
     ax.encoder.config.mode = 1 # ENCODER_MODE_HALL
     ax.encoder.config.cpr = 90
     ax.encoder.config.calib_scan_distance = 150
     ax.encoder.config.bandwidth = 100
     
-def configure_controllers(odrv, ax_num):
-    axes = [odrv.axis0, odrv.axis1]
-    ax = axes[ax_num]
+def configure_controllers(ax):
     ax.controller.config.pos_gain = 1
     torque_constant_estimate = 8
     ax.controller.config.vel_gain = 0.02 * torque_constant_estimate * ax.encoder.config.cpr
@@ -36,35 +31,63 @@ def configure_controllers(odrv, ax_num):
     ax.controller.config.control_mode = 1 # CONTROL_MODE_TORQUE_CONTROL
     ax.controller.config.enable_torque_mode_vel_limit = False
     
-def calibrate_motor(odrv, ax_num):
-    axes = [odrv.axis0, odrv.axis1]
-    ax = axes[ax_num]
+def encoder_error_to_string(error):
+    if error == odrive.enums.ENCODER_ERROR_ABS_SPI_COM_FAIL:
+        return "ENCODER_ERROR_ABS_SPI_COM_FAIL"
+    elif error == odrive.enums.ENCODER_ERROR_ABS_SPI_NOT_READY:
+        return "ENCODER_ERROR_ABS_SPI_NOT_READY"
+    elif error == odrive.enums.ENCODER_ERROR_ABS_SPI_TIMEOUT:
+        return "ENCODER_ERROR_ABS_SPI_TIMEOUT"
+    elif error == odrive.enums.ENCODER_ERROR_CPR_POLEPAIRS_MISMATCH:
+        return "ENCODER_ERROR_CPR_POLEPAIRS_MISMATCH"
+    elif error == odrive.enums.ENCODER_ERROR_HALL_NOT_CALIBRATED_YET:
+        return "ENCODER_ERROR_HALL_NOT_CALIBRATED_YET"
+    elif error == odrive.enums.ENCODER_ERROR_ILLEGAL_HALL_STATE:
+        return "ENCODER_ERROR_ILLEGAL_HALL_STATE"
+    elif error == odrive.enums.ENCODER_ERROR_INDEX_NOT_FOUND_YET:
+        return "ENCODER_ERROR_INDEX_NOT_FOUND_YET"
+    elif error == odrive.enums.ENCODER_ERROR_NONE:
+        return "ENCODER_ERROR_NONE"
+    elif error == odrive.enums.ENCODER_ERROR_NO_RESPONSE:
+        return "ENCODER_ERROR_NO_RESPONSE"
+    elif error == odrive.enums.ENCODER_ERROR_UNSTABLE_GAIN:
+        return "ENCODER_ERROR_UNSTABLE_GAIN"
+    elif error == odrive.enums.ENCODER_ERROR_UNSUPPORTED_ENCODER_MODE:
+        return "ENCODER_ERROR_UNSUPPORTED_ENCODER_MODE"
+
+# TODO(LuSeKa): Add motor and system error decoder methods.
+
+def calibrate_motor(ax):
     print("Calibrating motor...")
     ax.requested_state = 4 # AXIS_STATE_MOTOR_CALIBRATION
     time.sleep(1) # Wait for calibration to start.
     while ax.motor.is_armed:
         pass
     time.sleep(1)
-    # Todo(LuSeKa): Check for and handle possible errors.
-    ax.motor.config.pre_calibrated = True
+    phase_inductance = ax.motor.config.phase_inductance
+    phase_resistance = ax.motor.config.phase_resistance
+    print(f"Motor calibration result:\n\tPhase inductance: {phase_inductance}\n\tPhase resistance: {phase_resistance}")
+    return ax.motor.error
     
-def calibrate_encoder(odrv, ax_num):
-    axes = [odrv.axis0, odrv.axis1]
-    ax = axes[ax_num]    
+def calibrate_encoder_polarity(ax):
     print("Calibrating hall encoder polarity...")
     ax.requested_state = 12 # AXIS_STATE_ENCODER_HALL_POLARITY_CALIBRATION
     time.sleep(1) # Wait for calibration to start.
     while ax.motor.is_armed:
         pass
     time.sleep(1)
+    return ax.encoder.error
+
+def calibrate_encoder_offset(ax):
     print("Calibrating encoder offset...")
     ax.requested_state = 7 # AXIS_STATE_ENCODER_OFFSET_CALIBRATION
     time.sleep(1) # Wait for calibration to start.
     while ax.motor.is_armed:
         pass
     time.sleep(1)
-    # Todo(LuSeKa): Check for and handle possible errors.
-    ax.encoder.config.pre_calibrated = True
+    phase_offset = ax.encoder.config.phase_offset_float
+    print(f"Encoder calibration result:\n\tPhase offset: {phase_offset}")
+    return ax.encoder.error
 
 def main():
     parser = argparse.ArgumentParser(
@@ -84,6 +107,8 @@ def main():
     
     print("Connecting to ODrive...")
     odrv0 = odrive.find_any()
+    axes = [odrv0.axis0, odrv0.axis1]
+    ax = axes[ax_num]
     print("Setting parameters...")
     # Configure serial baudrate.
     odrv0.config.uart_a_baudrate = 115200
@@ -105,9 +130,9 @@ def main():
     odrv0.config.gpio4_mode = 0 # GPIO_MODE_DIGITAL
     odrv0.config.gpio5_mode = 0 # GPIO_MODE_DIGITAL
 
-    configure_motor(odrv0, ax_num)
-    configure_encoder(odrv0, ax_num)
-    configure_controllers(odrv0, ax_num)
+    configure_motor(ax)
+    configure_encoder(ax)
+    configure_controllers(ax)
     
     # Save configuration.
     try:
@@ -116,9 +141,24 @@ def main():
         print("Save and reboot...")
     # Re-discover ODrive.
     odrv0 = odrive.find_any()
-    calibrate_motor(odrv0, ax_num)
-    calibrate_encoder(odrv0, ax_num)    
-    
+    axes = [odrv0.axis0, odrv0.axis1]
+    ax = axes[ax_num]
+    motor_error = calibrate_motor(ax)
+    if motor_error:
+        sys.exit(f"Motor calibration failed with motor error {motor_error}. "\
+                 "Check the meaning here: https://docs.odriverobotics.com/v/latest/fibre_types/com_odriverobotics_ODrive.html#ODrive.Motor.Error")
+    ax.motor.config.pre_calibrated = True
+
+    encoder_error = calibrate_encoder_polarity(ax)
+    if encoder_error:
+        sys.exit(f"Encoder polarity calibration failed with encoder error {encoder_error_to_string(encoder_error)}.")
+
+    encoder_error = calibrate_encoder_offset(ax)
+    if encoder_error:
+        sys.exit(f"Encoder polarity calibration failed with encoder error {encoder_error_to_string(encoder_error)}.")
+
+    ax.encoder.config.pre_calibrated = True
+
     # Save configuration.
     try:
         odrv0.save_configuration()
@@ -127,11 +167,12 @@ def main():
     # Re-discover ODrive.
     odrv0 = odrive.find_any()
     system_error = odrv0.error
-    if not system_error:
-        print(f"Succesfully calibrated axis{ax_num}!")
-    else:
-        print(f"An error has occured: odrv0.error = {system_error}.")    
-    
+    if system_error:
+        sys.exit(f"Calibration failed with system error {system_error}. "\
+                 "Check the meaning here: https://docs.odriverobotics.com/v/latest/fibre_types/com_odriverobotics_ODrive.html#ODrive.Error")
+    print(f"Succesfully calibrated axis{ax_num}!")
+    sys.exit(0)
+
 if __name__ == "__main__":
     main()
 
