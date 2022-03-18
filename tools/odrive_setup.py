9555fdb637e203b366c60fd04c0fc5bac0acf440
Lukas Kaul <lukas.kaul@kit.edu>
2022-03-18 08:19:32 +0100

    Add motor and system error decoding to setup script.

--- tools/odrive_setup.py
+++ tools/odrive_setup.py
@@ -15,13 +15,15 @@ def configure_motor(ax):
     ax.motor.config.current_control_bandwidth = 100
     ax.motor.config.torque_constant = 1
     ax.motor.config.current_lim = 15
-    
+
+
 def configure_encoder(ax):
     ax.encoder.config.mode = 1 # ENCODER_MODE_HALL
     ax.encoder.config.cpr = 90
     ax.encoder.config.calib_scan_distance = 150
     ax.encoder.config.bandwidth = 100
-    
+
+
 def configure_controllers(ax):
     ax.controller.config.pos_gain = 1
     torque_constant_estimate = 8
@@ -30,7 +32,8 @@ def configure_controllers(ax):
     ax.controller.config.vel_limit = 10
     ax.controller.config.control_mode = 1 # CONTROL_MODE_TORQUE_CONTROL
     ax.controller.config.enable_torque_mode_vel_limit = False
-    
+
+
 def encoder_error_to_string(error):
     if error == odrive.enums.ENCODER_ERROR_ABS_SPI_COM_FAIL:
         return "ENCODER_ERROR_ABS_SPI_COM_FAIL"
@@ -54,8 +57,93 @@ def encoder_error_to_string(error):
         return "ENCODER_ERROR_UNSTABLE_GAIN"
     elif error == odrive.enums.ENCODER_ERROR_UNSUPPORTED_ENCODER_MODE:
         return "ENCODER_ERROR_UNSUPPORTED_ENCODER_MODE"
+    else:
+        return f"Unknown encoder error (error code {error})"
 
-# TODO(LuSeKa): Add motor and system error decoder methods.
+
+def motor_error_to_string(error):
+    if error == odrive.enums.MOTOR_ERROR_BAD_TIMING:
+        return "MOTOR_ERROR_BAD_TIMING"
+    elif error == odrive.enums.MOTOR_ERROR_BRAKE_RESISTOR_DISARMED:
+        return "MOTOR_ERROR_BRAKE_RESISTOR_DISARMED"
+    elif error == odrive.enums.MOTOR_ERROR_CONTROLLER_FAILED:
+        return "MOTOR_ERROR_CONTROLLER_FAILED"
+    elif error == odrive.enums.MOTOR_ERROR_CONTROLLER_INITIALIZING:
+        return "MOTOR_ERROR_CONTROLLER_INITIALIZING"
+    elif error == odrive.enums.MOTOR_ERROR_CONTROL_DEADLINE_MISSED:
+        return "MOTOR_ERROR_CONTROL_DEADLINE_MISSED"
+    elif error == odrive.enums.MOTOR_ERROR_CURRENT_LIMIT_VIOLATION:
+        return "MOTOR_ERROR_CURRENT_LIMIT_VIOLATION"
+    elif error == odrive.enums.MOTOR_ERROR_CURRENT_MEASUREMENT_UNAVAILABLE:
+        return "MOTOR_ERROR_CURRENT_MEASUREMENT_UNAVAILABLE"
+    elif error == odrive.enums.MOTOR_ERROR_CURRENT_SENSE_SATURATION:
+        return "MOTOR_ERROR_CURRENT_SENSE_SATURATION"
+    elif error == odrive.enums.MOTOR_ERROR_DRV_FAULT:
+        return "MOTOR_ERROR_DRV_FAULT"
+    elif error == odrive.enums.MOTOR_ERROR_FET_THERMISTOR_OVER_TEMP:
+        return "MOTOR_ERROR_FET_THERMISTOR_OVER_TEMP"
+    elif error == odrive.enums.MOTOR_ERROR_I_BUS_OUT_OF_RANGE:
+        return "MOTOR_ERROR_I_BUS_OUT_OF_RANGE"
+    elif error == odrive.enums.MOTOR_ERROR_MODULATION_IS_NAN:
+        return "MOTOR_ERROR_MODULATION_IS_NAN"
+    elif error == odrive.enums.MOTOR_ERROR_MODULATION_MAGNITUDE:
+        return "MOTOR_ERROR_MODULATION_MAGNITUDE"
+    elif error == odrive.enums.MOTOR_ERROR_MOTOR_THERMISTOR_OVER_TEMP:
+        return "MOTOR_ERROR_MOTOR_THERMISTOR_OVER_TEMP"
+    elif error == odrive.enums.MOTOR_ERROR_NONE:
+        return "MOTOR_ERROR_NONE"
+    elif error == odrive.enums.MOTOR_ERROR_PHASE_INDUCTANCE_OUT_OF_RANGE:
+        return "MOTOR_ERROR_PHASE_INDUCTANCE_OUT_OF_RANGE"
+    elif error == odrive.enums.MOTOR_ERROR_PHASE_RESISTANCE_OUT_OF_RANGE:
+        return "MOTOR_ERROR_PHASE_RESISTANCE_OUT_OF_RANGE"
+    elif error == odrive.enums.MOTOR_ERROR_SYSTEM_LEVEL:
+        return "MOTOR_ERROR_SYSTEM_LEVEL"
+    elif error == odrive.enums.MOTOR_ERROR_TIMER_UPDATE_MISSED:
+        return "MOTOR_ERROR_TIMER_UPDATE_MISSED"
+    elif error == odrive.enums.MOTOR_ERROR_UNBALANCED_PHASES:
+        return "MOTOR_ERROR_UNBALANCED_PHASES"
+    elif error == odrive.enums.MOTOR_ERROR_UNKNOWN_CURRENT_COMMAND:
+        return "MOTOR_ERROR_UNKNOWN_CURRENT_COMMAND"
+    elif error == odrive.enums.MOTOR_ERROR_UNKNOWN_CURRENT_MEASUREMENT:
+        return "MOTOR_ERROR_UNKNOWN_CURRENT_MEASUREMENT"
+    elif error == odrive.enums.MOTOR_ERROR_UNKNOWN_GAINS:
+        return "MOTOR_ERROR_UNKNOWN_GAINS"
+    elif error == odrive.enums.MOTOR_ERROR_UNKNOWN_PHASE_ESTIMATE:
+        return "MOTOR_ERROR_UNKNOWN_PHASE_ESTIMATE"
+    elif error == odrive.enums.MOTOR_ERROR_UNKNOWN_PHASE_VEL:
+        return "MOTOR_ERROR_UNKNOWN_PHASE_VEL"
+    elif error == odrive.enums.MOTOR_ERROR_UNKNOWN_TORQUE:
+        return "MOTOR_ERROR_UNKNOWN_TORQUE"
+    elif error == odrive.enums.MOTOR_ERROR_UNKNOWN_VBUS_VOLTAGE:
+        return "MOTOR_ERROR_UNKNOWN_VBUS_VOLTAGE"
+    elif error == odrive.enums.MOTOR_ERROR_UNKNOWN_VOLTAGE_COMMAND:
+        return "MOTOR_ERROR_UNKNOWN_VOLTAGE_COMMAND"
+    else:
+        return f"Unknown motor error (error code {error})"
+
+
+def odrive_error_to_string(error):
+    if error == odrive.enums.ODRIVE_ERROR_BRAKE_DEADTIME_VIOLATION:
+        return "ODRIVE_ERROR_BRAKE_DEADTIME_VIOLATION"
+    elif error == odrive.enums.ODRIVE_ERROR_BRAKE_DUTY_CYCLE_NAN:
+        return "ODRIVE_ERROR_BRAKE_DUTY_CYCLE_NAN"
+    elif error == odrive.enums.ODRIVE_ERROR_CONTROL_ITERATION_MISSED:
+        return "ODRIVE_ERROR_CONTROL_ITERATION_MISSED"
+    elif error == odrive.enums.ODRIVE_ERROR_DC_BUS_OVER_CURRENT:
+        return "ODRIVE_ERROR_DC_BUS_OVER_CURRENT"
+    elif error == odrive.enums.ODRIVE_ERROR_DC_BUS_OVER_REGEN_CURRENT:
+        return "ODRIVE_ERROR_DC_BUS_OVER_REGEN_CURRENT"
+    elif error == odrive.enums.ODRIVE_ERROR_DC_BUS_OVER_VOLTAGE:
+        return "ODRIVE_ERROR_DC_BUS_OVER_VOLTAGE"
+    elif error == odrive.enums.ODRIVE_ERROR_DC_BUS_UNDER_VOLTAGE:
+        return "ODRIVE_ERROR_DC_BUS_UNDER_VOLTAGE"
+    elif error == odrive.enums.ODRIVE_ERROR_INVALID_BRAKE_RESISTANCE:
+        return "ODRIVE_ERROR_INVALID_BRAKE_RESISTANCE"
+    elif error == odrive.enums.ODRIVE_ERROR_NONE:
+        return "ODRIVE_ERROR_NONE"
+    else:
+        return f"Unknown odrive error (error code {error})"
+
 
 def calibrate_motor(ax):
     print("Calibrating motor...")
@@ -68,7 +156,8 @@ def calibrate_motor(ax):
     phase_resistance = ax.motor.config.phase_resistance
     print(f"Motor calibration result:\n\tPhase inductance: {phase_inductance}\n\tPhase resistance: {phase_resistance}")
     return ax.motor.error
-    
+
+
 def calibrate_encoder_polarity(ax):
     print("Calibrating hall encoder polarity...")
     ax.requested_state = 12 # AXIS_STATE_ENCODER_HALL_POLARITY_CALIBRATION
@@ -77,6 +166,7 @@ def calibrate_encoder_polarity(ax):
         pass
     time.sleep(1)
     return ax.encoder.error
+
 
 def calibrate_encoder_offset(ax):
     print("Calibrating encoder offset...")
@@ -88,6 +178,7 @@ def calibrate_encoder_offset(ax):
     phase_offset = ax.encoder.config.phase_offset_float
     print(f"Encoder calibration result:\n\tPhase offset: {phase_offset}")
     return ax.encoder.error
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -145,8 +236,7 @@ def main():
     ax = axes[ax_num]
     motor_error = calibrate_motor(ax)
     if motor_error:
-        sys.exit(f"Motor calibration failed with motor error {motor_error}. "\
-                 "Check the meaning here: https://docs.odriverobotics.com/v/latest/fibre_types/com_odriverobotics_ODrive.html#ODrive.Motor.Error")
+        sys.exit(f"Motor calibration failed with motor error {motor_error_to_string(motor_error)}.")
     ax.motor.config.pre_calibrated = True
 
     encoder_error = calibrate_encoder_polarity(ax)
@@ -155,7 +245,7 @@ def main():
 
     encoder_error = calibrate_encoder_offset(ax)
     if encoder_error:
-        sys.exit(f"Encoder polarity calibration failed with encoder error {encoder_error_to_string(encoder_error)}.")
+        sys.exit(f"Encoder offset calibration failed with encoder error {encoder_error_to_string(encoder_error)}.")
 
     ax.encoder.config.pre_calibrated = True
 
@@ -168,10 +258,10 @@ def main():
     odrv0 = odrive.find_any()
     system_error = odrv0.error
     if system_error:
-        sys.exit(f"Calibration failed with system error {system_error}. "\
-                 "Check the meaning here: https://docs.odriverobotics.com/v/latest/fibre_types/com_odriverobotics_ODrive.html#ODrive.Error")
+        sys.exit(f"Calibration failed with system error {odrive_error_to_string(system_error)}.")
     print(f"Succesfully calibrated axis{ax_num}!")
     sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
